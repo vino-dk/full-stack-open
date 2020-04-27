dbbba31f3ee92f6b9bdee6f9786c80ef1b6c7575
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2020-04-27 15:13:45 +0200

    checkpoint 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function App() {
-  return (
+const App = () => {
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return(
     <div>
-      Program 1
+      <h1>How well did we do?</h1> 
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h2>Statistics</h2>
+      <p>Good: {good} </p>
+      <p>Neutral: {neutral} </p>
+      <p>Bad: {bad} </p>
     </div>
-  );
+  )
 }
 
 export default App;
