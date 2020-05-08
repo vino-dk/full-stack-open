20f4e993a4b89dcba3dfc99608909aba1988cc0a
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2020-05-08 17:48:29 +0200

    checkpoint 2.9

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -2,27 +2,36 @@ import React, { useState } from 'react';
 import Person from './components/Person';
 
 const App = () => {
-  const [persons, setPersons] = useState([{id: null}])
+  const [persons, setPersons] = useState(
+    [
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ]
+  )
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', newName)
-    
+
     let nameFound = false;
     for (let i = 0; i < persons.length; i++) {
-      if (persons[i].name === newName) 
+      if (persons[i].name === newName)
         nameFound = true;
+      break;
     }
-    if(!nameFound){
+    if (!nameFound) {
       const personObject = {
         name: newName,
         id: persons.length + 1
       }
       setPersons(persons.concat(personObject))
       setNewName('')
-    } else{
+    } else {
       window.alert(`${newName} is already added to phonebook`);
     }
   }
@@ -32,10 +41,10 @@ const App = () => {
     let nameFound = false;
 
     for (let i = 0; i < persons.length; i++) {
-      if (persons[i].name === newName) 
+      if (persons[i].name === newName)
         nameFound = true;
     }
-    if(!nameFound){
+    if (!nameFound) {
       const personObject = {
         name: newName,
         number: newNumber,
@@ -44,7 +53,7 @@ const App = () => {
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
-    } else{
+    } else {
       window.alert(`${newName} is already added to phonebook`);
     }
   }
@@ -59,10 +68,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterOnChange = (event) => {
+    console.log(event.target.value)
+    setFilterName(event.target.value)
+
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
 
+      <div>
+        filter by:
+        <input
+          value={filterName}
+          onChange={handleFilterOnChange}
+        />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={(addPerson, addNumber)}>
         <div>
           name:
@@ -70,12 +95,12 @@ const App = () => {
             value={newName}
             onChange={handleNameOnChange}
           />
-        <div>
-          number: <input 
-          value={newNumber}
-          onChange={handleNumberOnChange}
-          />
-        </div>
+          <div>
+            number: <input
+              value={newNumber}
+              onChange={handleNumberOnChange}
+            />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -83,7 +108,10 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {persons.filter(person => person.name
+        .toLowerCase()
+        .trim()
+        .search(filterName.toLowerCase().trim()) >= 0).map(person =>
           <Person key={person.id} person={person} />
         )}
     </div>
